9a9af184099c8a7c93f6811f3c58bb3923febf69
Buce <dmbuce@gmail.com>
2013-03-01 20:19:34 -0500

    Update copyright

--- norbert.py
+++ norbert.py
@@ -2,7 +2,7 @@
 #
 #   norbert.py - command line NBT editor
 #
-#   Copyright (C) 2012 DMBuce <dmbuce@gmail.com>
+#   Copyright (C) 2012-2013 DMBuce <dmbuce@gmail.com>
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
